0ce5a038d83a3db3b4dc84e56ba0e9d802f6d2f3
Amol Dumbre <amol.dumbre>
2020-02-23 15:53:41 +0530

    Added heroku config

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import logging
 from flask_cors import CORS
 
 app = Flask('sms-analytics')
-# app.secret_key = "secret key"
+app.secret_key = "gp73LzuvRM"
 
 if app.config["ENV"] == "production":
-  app.config["MONGO_URI"] = "mongodb://localhost:27017/sms_analytics"
+  app.config["MONGO_URI"] = ENV['MONGODB_URI']
   logging.basicConfig(filename='log/production.log',level=logging.INFO)
 else:
   logging.basicConfig(filename='log/development.log',level=logging.DEBUG)
   app.config["MONGO_URI"] = "mongodb://localhost:27017/sms_analytics"
-  
+
 # logging.debug("---ENV--------APP-{}---".format(app.config["ENV"]))
 
 mongo = PyMongo(app)
