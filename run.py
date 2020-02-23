d3d645fa47a8c67ec59d4c9cc114b044710c3cf5
Amol Dumbre <amol.dumbre>
2020-02-23 20:52:20 +0530

    Added MONGO URI

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def otplist():
   otplist = OTPLIST_TABLE.find(sort=[( 'created_at', pymongo.DESCENDING )] ).limit(5)
   return render_template("otplist.html", otplist = otplist)
 
-@app.route('/api/add_otp', methods=['POST'])
+@app.route('/api/otp', methods=['POST'])
 def addotp():
   # import pdb; pdb.set_trace()
   try:
@@ -28,15 +28,12 @@ def addotp():
     else:   
       otp_token = request.json['otp_token']
       id = OTPLIST_TABLE.insert({'otp_token': otp_token, "created_at": datetime.now()})
-
       resp = jsonify({'message':'Token added successfully!'})
       resp.status_code = 200
       return resp
   except Exception as e:
-    logging.debug("-------EXCEPTION---{}---".format(e.args))
+    logging.debug("-------EXCEPTION-in--{}---".format(e.args))
     return jsonify({'message': "Exception occured ".format(e.args) })
-  # else:
-  #   return not_found()
 
 @app.errorhandler(404)
 def page_not_found(error):
